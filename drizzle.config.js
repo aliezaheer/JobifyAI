86b12b769990fd000ca9f4769c92298329c92a38
Ali Zaheer <alizaheer67@gmail.com>
2025-01-08 16:53:41 +0500

    schema setup for app

--- drizzle.config.js
+++ drizzle.config.js
@@ -2,7 +2,7 @@
 
 
 export default ({
-    schema: "./src/schema.ts",
+    schema: "./utils/schema.js",
   dialect: "postgresql",
  dbCredentials: {
     url: 'postgresql://neondb_owner:Y8mkBw0zoLqc@ep-muddy-shape-a57amnw6.us-east-2.aws.neon.tech/neondb?sslmode=require',
--- utils/db.js
+++ utils/db.js
@@ -3,4 +3,4 @@ import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 config({ path: ".env" }); 
 const sql = neon(process.env.NEXT_PUBLIC_DRIZZLE_DB_URL);
-export const db = drizzle({ client: sql });
+export const db = drizzle( sql,{schema} );
