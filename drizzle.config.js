901e1bb40ce09171134350e7aaedff0d0f6d80b5
Ali Zaheer <alizaheer67@gmail.com>
2025-01-08 17:00:05 +0500

    env fixed

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default ({
     schema: "./utils/schema.js",
   dialect: "postgresql",
  dbCredentials: {
-    url: 'postgresql://neondb_owner:Y8mkBw0zoLqc@ep-muddy-shape-a57amnw6.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
  }
 });
